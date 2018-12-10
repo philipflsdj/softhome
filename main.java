fb4c5f0c13f51578dc3ff5af95ebbca13aa0640f
philippe.oliiveria <44622081+philipflsdj@users.noreply.github.com>
2018-12-10 14:16:54 -0200

    Add files via upload

---
+++ main.java
@@ -0,0 +1,6 @@
+package softhome;
+public class main {
+    public static void main(String[] args) {
+    }
+    
+}
---
+++ usable.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package softhome;
+
+/**
+ *
+ * @author philippe.oliveira.a
+ */
+public interface usable {
+    
+}
